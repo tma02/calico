34d85171b7bd39ec23232965ea05456d9c5ebb97
Tianyi Ma <tn.ma@outlook.com>
2024-11-25 21:23:52 -0800

    Change PhysicsPatchEnabled default to false

--- Teemaw.Calico/Config.cs
+++ Teemaw.Calico/Config.cs
@@ -6,6 +6,6 @@ public class Config
 {
     [JsonInclude] public bool NetworkPatchEnabled = true;
     [JsonInclude] public bool PlayerPatchEnabled = true;
-    [JsonInclude] public bool PhysicsPatchEnabled = true;
+    [JsonInclude] public bool PhysicsPatchEnabled = false;
     [JsonInclude] public bool RemoveDisconnectedPlayerProps = true;
 }
