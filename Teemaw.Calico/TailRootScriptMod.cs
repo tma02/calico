78e4e05bcc9f5978f0bbfda0758a527c9f0ebbe7
Tianyi Ma <tn.ma@outlook.com>
2024-11-28 15:45:27 -0800

    Fix tail attachment

--- Teemaw.Calico/TailRootScriptMod.cs
+++ Teemaw.Calico/TailRootScriptMod.cs
@@ -16,10 +16,24 @@ public class TailRootScriptMod(IModInterface mod): IScriptMod
         
         """);
     
+    private static readonly IEnumerable<Token> OnReady = ScriptTokenizer.Tokenize(
+        """
+
+        process_priority = 1 + get_parent().process_priority
+
+        """, 1);
+    
     public bool ShouldRun(string path) => path == "res://Scenes/Entities/Player/tail_root.gdc";
 
     public IEnumerable<Token> Modify(string path, IEnumerable<Token> tokens)
     {
+        MultiTokenWaiter readyWaiter = new([
+            t => t is { Type: PrFunction },
+            t => t is IdentifierToken { Name: "_ready" },
+            t => t.Type is ParenthesisOpen,
+            t => t.Type is ParenthesisClose,
+            t => t.Type is Colon,
+        ]);
         MultiTokenWaiter extendsWaiter = new([
             t => t.Type is PrExtends,
             t => t.Type is Identifier,
@@ -31,6 +45,7 @@ public class TailRootScriptMod(IModInterface mod): IScriptMod
         var patchFlags = new Dictionary<string, bool>
         {
             ["globals"] = false,
+            ["ready"] = false,
         };
         
         foreach (var t in tokens)
@@ -41,6 +56,13 @@ public class TailRootScriptMod(IModInterface mod): IScriptMod
                 foreach (var t1 in Globals) yield return t1;
                 patchFlags["globals"] = true;
                 mod.Logger.Information("[calico.TailRootScriptMod] globals patch OK");
+            }
+            else if (readyWaiter.Check(t))
+            {
+                yield return t;
+                foreach (var t1 in OnReady) yield return t1;
+                patchFlags["ready"] = true;
+                mod.Logger.Information("[calico.TailRootScriptMod] ready patch OK");
             }
             else
             {
