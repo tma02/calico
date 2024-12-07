bf47535631cf71b130a2213cea7b92c52e1cc830
Tianyi Ma <tn.ma@outlook.com>
2024-12-07 14:18:43 -0800

    Use builders in GuitarStringSoundScriptModFactory

--- Teemaw.Calico/ScriptMod/GuitarStringSoundScriptModFactory.cs
+++ Teemaw.Calico/ScriptMod/GuitarStringSoundScriptModFactory.cs
@@ -1,10 +1,8 @@
 ﻿using GDWeave;
 using GDWeave.Modding;
 using Teemaw.Calico.LexicalTransformer;
-using Teemaw.Calico.Util;
 using static Teemaw.Calico.LexicalTransformer.Operation;
 using static Teemaw.Calico.LexicalTransformer.TransformationPatternFactory;
-using ScriptTokenizer = Teemaw.Calico.Util.ScriptTokenizer;
 
 namespace Teemaw.Calico.ScriptMod;
 
@@ -12,40 +10,66 @@ public static class GuitarStringSoundScriptModFactory
 {
     public static IScriptMod Create(IModInterface mod)
     {
-        return new TransformationRuleScriptMod(mod, "GuitarStringSoundScriptMod",
-            "res://Scenes/Entities/Player/guitar_string_sound.gdc", [
-                new TransformationRule("globals", CreateGlobalsPattern(),
-                    ScriptTokenizer.Tokenize(
-                        """
+        return new TransformationRuleScriptModBuilder()
+            .ForMod(mod)
+            .Named("GuitarStringSoundScriptMod")
+            .Patching("res://Scenes/Entities/Player/guitar_string_sound.gdc")
+            .AddRule(new TransformationRuleBuilder()
+                .Named("globals")
+                .Matching(CreateGlobalsPattern())
+                .Do(Append)
+                .With(
+                    """
 
-                        var calico_playing_count = 0
+                    var calico_playing_count = 0
 
-                        """)),
-                new TransformationRule("add_child_in_ready", CreateGdSnippetPattern("add_child(new)"), [],
-                    ReplaceAll),
-                new TransformationRule("call_guard", CreateFunctionDefinitionPattern("_call"),
-                    ScriptTokenizer.Tokenize(
-                        """
+                    """
+                )
+            )
+            .AddRule(new TransformationRuleBuilder()
+                .Named("add_child_in_ready")
+                .Matching(CreateGdSnippetPattern("add_child(new)"))
+                .Do(ReplaceAll)
+                .With([])
+            )
+            .AddRule(new TransformationRuleBuilder()
+                .Named("call_guard")
+                .Matching(CreateFunctionDefinitionPattern("_call"))
+                .Do(Append)
+                .With(
+                    """
 
-                        if calico_playing_count == 0: return
+                    if calico_playing_count == 0: return
 
-                        """, 1)),
-                new TransformationRule("node_play", CreateGdSnippetPattern("node.play(point)"),
-                    ScriptTokenizer.Tokenize(
-                        """
+                    """, 1
+                )
+            )
+            .AddRule(new TransformationRuleBuilder()
+                .Named("node_play")
+                .Matching(CreateGdSnippetPattern("node.play(point)"))
+                .Do(Prepend)
+                .With(
+                    """
 
-                        add_child(node)
-                        calico_playing_count += 1
+                    add_child(node)
+                    calico_playing_count += 1
 
-                        """, 3), Prepend),
-                new TransformationRule("node_stopped", CreateGdSnippetPattern("sound.playing = false"),
-                    ScriptTokenizer.Tokenize(
-                        """
+                    """, 3
+                )
+            )
+            .AddRule(new TransformationRuleBuilder()
+                .Named("node_stopped")
+                .Matching(CreateGdSnippetPattern("sound.playing = false"))
+                .Do(Append)
+                .With(
+                    """
 
-                        remove_child(sound)
-                        calico_playing_count -= 1
+                    remove_child(sound)
+                    calico_playing_count -= 1
 
-                        """, 3)),
-            ]);
+                    """, 3
+                )
+            )
+            .Build();
     }
 }
